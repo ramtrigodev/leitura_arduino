9d217ef6268678a1043ab6607246c71df7905ecd
Ramon Trigo <121834069+ramtrigodev@users.noreply.github.com>
2025-04-03 11:21:12 -0300

    envio

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import ArduinoSerialReader from "./leitura.js";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <ArduinoSerialReader />
+    </div>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
